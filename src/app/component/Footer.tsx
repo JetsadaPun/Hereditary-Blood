4a02b9188d591cac8db33ee52e27f36c23b3d175
Aomang <ketsaraphon15vip@gmail.com>
2024-10-10 00:11:17 +0700

    first commit

--- src/app/component/Footer.tsx
+++ src/app/component/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="border-t-2 border-mainred text-mainred text-center  py-4">
+    <footer className="border-t-2 border-mainred text-mainred text-center  py-4 relative  bottom-0 w-full">
       copyright 2025 | Group 5
     </footer>
   );
